0988343e5161847e4b5d2b71f596c813996fca97
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-04-13 17:24:45 +0300

    CORS credential support

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Разрешаем учетные данные
   })
 );
 
