f70911e3a6360fd48e80a4018899e729c8c1cb79
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-04-13 17:12:08 +0300

    done

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 import express from "express";
-
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
-
 import { connectDB } from "./database/mongodb.js";
 import userRoutes from "./routes/users.route.js";
-
 
 const port = PORT || 8080;
 const app = express();
 
-app.use(express.json());           
-app.use(express.urlencoded({ extended: true })); 
-app.get('/', (req, res) => {
-    res.send('Hello server!');
-});
+// Настройка CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Простой маршрут
+app.get("/", (req, res) => {
+  res.send("Hello server!");
+});
 
-//Routes
+// Маршруты API
 app.use("/api/v1/users", userRoutes);
 
+// Запуск сервера
 app.listen(port, async () => {
-    console.log(`Server started on http://localhost:${port}`);
-    await connectDB();
+  console.log(`Server started on http://localhost:${port}`);
+  await connectDB();
 });
